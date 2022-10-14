b8e833a307c020c7e9f0835b8b18dec84c4f987a
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2022-10-14 16:28:18 +0100

    Update SakilaConfig.java

--- SakilaWeb/src/main/java/com/sparta/sakilaweb/config/SakilaConfig.java
+++ SakilaWeb/src/main/java/com/sparta/sakilaweb/config/SakilaConfig.java
@@ -33,8 +33,6 @@ public class SakilaConfig extends WebSecurityConfigurerAdapter {
                 .hasAnyAuthority("ADMIN")
                 .antMatchers("/sakila/actor/*")
                 .permitAll()
-                .antMatchers("/asdad")
-                .access()
                 .anyRequest()
                 .authenticated();
         http.formLogin()
